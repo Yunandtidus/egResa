844efe747e6d1a33502be52572740cba47f09784
daniel pervez <frenchsan@free.fr>
2017-10-01 17:21:33 +0200

    add planning[j][i] object in calendar-details.component

--- src/app/calendar-details/calendar-details.component.ts
+++ src/app/calendar-details/calendar-details.component.ts
@@ -7,34 +7,42 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./calendar-details.component.less']
 })
 export class CalendarDetailsComponent implements OnInit {
+    choice: string;
+
 
 @Input() hour:number;
 @Input() day: Date;
-@Input() state:String;
-
-
+@Input() state;
 
 
   constructor( private roomService: HttpRoomService) { }
 
   ngOnInit() {   
-
+       
   }
 
    addSession(day: Date, hour: number) {
-        console.log("ajout de session" + day + hour);
-        let d: Date = new Date();
-        d.setTime(day.getTime());
-        d.setHours(Math.round(hour));
-        d.setMinutes((hour - Math.round(hour)) * 60);
-        d.setSeconds(0);
-        d.setMilliseconds(0);
-        this.roomService.addSession(1, d, 90)
-            .subscribe(
-                result => {this.state="reservable"; console.log("ok", result); }, 
-                e => { console.log(e, "ko")}                
-            );
-        //this.onChange()(this.currentDate);
+        // console.log("ajout de session" + day + hour);
+        // let d: Date = new Date();
+        // d.setTime(day.getTime());
+        // d.setHours(Math.round(hour));
+        // d.setMinutes((hour - Math.round(hour)) * 60);
+        // d.setSeconds(0);
+        // d.setMilliseconds(0);
+        // this.roomService.addSession(1, d, 90)
+        //     .subscribe(
+        //         result => {this.mycolor="moyen"; this.state.etat="reservable"; console.log("ok", result); }, 
+        //         e => { console.log(e, "ko")}                
+        //     );
+        this.choice = "selected";
+      
+    }
+
+    createSession(){
+    // [routerLink]="['/reservation']"
+    console.log(this.hour);
+    console.log(this.day);   
+    
     }
 
 }
